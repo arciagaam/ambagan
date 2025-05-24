e187bffc732ec33800b5436558ca8e8e27a00c92
meynardtrinidad <meynard.trinidad44@gmail.com>
2025-05-24 15:14:54 +0800

    fix: missing modules

--- app/api/lib/utils/validateRequestBody.ts
+++
@@ -1,9 +0,0 @@
-import { ZodSchema } from "zod";
-
-export const validateRequestBody = async (schema: ZodSchema, body: unknown) => {
-  try {
-    schema.parse(body);
-  } catch (error) {
-    return { statusMessage: "Bad Request", statusCode: 400, error };
-  }
-};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import QueryClientContainer from "./_components/QueryClientProvider";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// import localFont from "next/font/local";
+// 
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Ambagan!",
-  description: "Bill splitting application",
+  description: "A bill splitting application",
 };
-
 
 export default function RootLayout({
   children,
@@ -27,15 +25,53 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <QueryClientContainer>
-        <html lang="en">
-          <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-            <main>
-              {children}
-              <ReactQueryDevtools initialIsOpen={false} />
-            </main>
-          </body>
-        </html>
-      </QueryClientContainer>
+    <html lang="en">
+      <body className={`antialiased`}>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import localFont from "next/font/local";
+// import "./globals.css";
+// import QueryClientContainer from "./_components/QueryClientProvider";
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// 
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+// 
+// export const metadata: Metadata = {
+//   title: "Ambagan!",
+//   description: "Bill splitting application",
+// };
+// 
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <QueryClientContainer>
+//       <html lang="en">
+//         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+//           <main>
+//             {children}
+//             <ReactQueryDevtools initialIsOpen={false} />
+//           </main>
+//         </body>
+//       </html>
+//     </QueryClientContainer>
+//   );
+// }
